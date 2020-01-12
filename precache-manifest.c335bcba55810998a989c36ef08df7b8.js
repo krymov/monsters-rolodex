dd623c5111b597f16755595f8d7bebbb75c6335a
Mark Krymov <mark@memco.ru>
2020-01-12 13:12:13 +0100

    Updates

---
+++ precache-manifest.c335bcba55810998a989c36ef08df7b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222902ee57d14d6f63c8b97a1b28f57f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36db252c9e19ac49160b",
+    "url": "/monsters-rolodex/static/css/main.eda6532c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36db252c9e19ac49160b",
+    "url": "/monsters-rolodex/static/js/main.f5a241fc.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
